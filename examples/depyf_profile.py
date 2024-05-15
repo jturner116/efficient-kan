8c7685afbf4455fe1dea5ba4e2aaccc61719b3cf
Jonah Turner <jonahturner2000@hotmail.com>
2024-05-15 11:22:57 +0200

    clean

--- examples/depyf_profile.py
+++ examples/depyf_profile.py
@@ -4,24 +4,24 @@ from efficient_kan import KAN
 from torch.profiler import profile, record_function, ProfilerActivity
 import depyf
 
-
 torch.set_float32_matmul_precision("high")
 
 model = KAN([28 * 28, 64, 10])
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model.to(device)
-model = torch.compile(model)
+compiled_model = torch.compile(model)
 
 # Warmup
 for _ in range(10):
-    model(torch.randn(64, 28 * 28).to(device))
+    compiled_model(torch.randn(64, 28 * 28).to(device))
 
 
-def main():
+def main(inputs):
     for _ in range(100):
-        model(torch.randn(64, 28 * 28).to(device))
+        compiled_model(inputs)
 
 
 if __name__ == "__main__":
+    inputs = torch.randn(64, 28 * 28).to(device)
     with depyf.prepare_debug("./depyf_debug"):
-        main()
+        main(inputs)
